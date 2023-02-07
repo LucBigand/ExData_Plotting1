6fb26c793ccfdfd17188f9e541b41670a386ac74
Luc Bigand <lucbigand@yahoo.fr>
2023-02-07 15:49:54 +0100

    Add legend and correct axis labels on plot3

--- plot3.R
+++ plot3.R
@@ -4,8 +4,11 @@ png("plot3.png")
 
 times <- as.POSIXct(paste(data$Date, data$Time), 
 	              format = "%d/%m/%Y %H:%M:%OS")
-plot(times, data$Sub_metering_1, type = "l", col = "green")
+plot(times, data$Sub_metering_1, type = "l", col = "green", xlab = "datetime",
+     ylab = "Energy sub metering")
 lines(times, data$Sub_metering_2, type = "l", col = "red")
 lines(times, data$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+      col = c("green", "red", "blue"), lwd = 1)
 
 dev.off()
